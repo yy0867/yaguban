91a1f8baeedd1f2106a8b6541024fc6d9b76b1b2
yy0867 <yy0867@naver.com>
2021-01-29 02:33:18 +0900

    add Bottom Navigation View

--- app/src/main/java/com/harry/yaguban/MainActivity.java
+++ app/src/main/java/com/harry/yaguban/MainActivity.java
@@ -1,14 +1,44 @@
 package com.harry.yaguban;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(this, SplashActivity.class));
+
+        BottomNavigationView navView = findViewById(R.id.bottom_nav_view);
+        navView.setOnNavigationItemSelectedListener(this);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()) {
+            case R.id.bottom_nav_batter:
+                Toast.makeText(this, "batter!", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.bottom_nav_pitcher:
+                Toast.makeText(this, "pitcher!", Toast.LENGTH_SHORT).show();
+                return true;
+            case R.id.bottom_nav_add_player:
+                Toast.makeText(this, "add player!", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+
+        return false;
     }
 }
---
+++ app/src/main/java/com/harry/yaguban/SplashActivity.java
@@ -0,0 +1,30 @@
+package com.harry.yaguban;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler();
+        startLoading();
+    }
+
+    private void startLoading() {
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 2000);
+    }
+}
