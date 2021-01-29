2afdf9bbac058ffbdf9589d487c5e3131ee615de
yy0867 <yy0867@naver.com>
2021-01-29 12:29:43 +0900

    Crate Fragment

--- app/src/main/java/com/harry/yaguban/MainActivity.java
+++ app/src/main/java/com/harry/yaguban/MainActivity.java
@@ -2,8 +2,10 @@ package com.harry.yaguban;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.annotation.SuppressLint;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -11,7 +13,12 @@ import android.widget.Toast;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+public class MainActivity extends AppCompatActivity {
+
+    Fragment batterListFragment;
+    Fragment pitcherListFragment;
+    Fragment addPlayerFragment;
+    BottomNavigationView navView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,25 +27,36 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
 
         startActivity(new Intent(this, SplashActivity.class));
 
-        BottomNavigationView navView = findViewById(R.id.bottom_nav_view);
-        navView.setOnNavigationItemSelectedListener(this);
+        //Create Object
+        batterListFragment = new BatterListFragment();
+        pitcherListFragment = new PitcherListFragment();
+        addPlayerFragment = new AddPlayerFragment();
+        navView = findViewById(R.id.bottom_nav_view);
+
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener);
+
+        //Fragment Manager
+        getSupportFragmentManager().beginTransaction().replace(R.id.layout_fragment_container, batterListFragment).commit();
     }
 
-    @SuppressLint("NonConstantResourceId")
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch(item.getItemId()) {
-            case R.id.bottom_nav_batter:
-                Toast.makeText(this, "batter!", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.bottom_nav_pitcher:
-                Toast.makeText(this, "pitcher!", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.bottom_nav_add_player:
-                Toast.makeText(this, "add player!", Toast.LENGTH_SHORT).show();
-                return true;
+    private final BottomNavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @SuppressLint("NonConstantResourceId")
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch(item.getItemId()) {
+                case R.id.bottom_nav_batter:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.layout_fragment_container, batterListFragment).commit();
+                    return true;
+                case R.id.bottom_nav_pitcher:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.layout_fragment_container, pitcherListFragment).commit();
+                    return true;
+                case R.id.bottom_nav_add_player:
+                    getSupportFragmentManager().beginTransaction().replace(R.id.layout_fragment_container, addPlayerFragment).commit();
+                    return true;
+            }
+
+            return false;
         }
-
-        return false;
-    }
+    };
 }
